7006806c32b685ff4fa5c3e445b2981be09a5dc9
Jörg Will <jw@winback.de>
2019-01-15 13:55:42 +0100

    Web-Server verbessert

--- esp8266_WLN/webServer_WLN.ino
+++ esp8266_WLN/webServer_WLN.ino
@@ -16,62 +16,91 @@ void loopWeb() {
 
 void handleXML() {
 	Serial.print("XML-Abfrage");
-	webserver.send(200, "text/xml", String(weight));
+	webserver.send(200, "text/xml", String(weight) + " kg");
 }
 
 void handleIndexPage(void) {
 	Serial.print("Web-Abfrage");
 
 	pageContent = "<!DOCTYPE html>";
-	pageContent += "<html lang=\"de\">";
+	pageContent += "<html lang='de'>";
 	pageContent += "<head>";
-	pageContent += "<meta charset=\"UTF-8\" />";
+	pageContent += "<meta charset='UTF-8' />";
 	pageContent += "<title>WinBack</title>";
+	pageContent += "<meta name = 'viewport' content = 'width=device-width, initial-scale=1'>";
 
-	pageContent += "<meta name = \"viewport\" content = \"width=device-width, initial-scale=1\">";
 	pageContent += "<style>";
-	pageContent += "body{font-family: Arial, Helvetica, sans-serif;margin:0;}";
+	pageContent += "*{box-sizing:border-box;}";
+	pageContent += "body{font-family:Arial, Helvetica, sans-serif; margin:0;}";
 
 	/* Style the header */
-	pageContent += ".header{padding: 80px;text-align: center;background: #1abc9c;color: white;}";
+	pageContent += ".header{padding:1px; text-align:center; background:#1abc9c; color:white;}";
 	/* Increase the font size of the h1 element */
-	pageContent += ".header h1{font-size: 40px;}";
+	pageContent += ".header h1{font-size:80px;}";
+	/* Footer */
+	pageContent += ".footer{padding:1px; text-align:left ;background:#ddd; margin-top:0px; font-size:10px;}";
+
+	/* Left column */
+	pageContent += ".leftcolumn{float:left; width:75%;}";
+	/* Right column */
+	pageContent += ".rightcolumn{float:right; width:25%; padding-left:20px;}";
+	/* Clear floats after the columns */
+	pageContent += ".row:after{content:''; display:table; clear:both;}";
 
 	/* Style the top navigation bar */
-	pageContent += ".navbar{overflow: hidden;background-color: #333;}";
+	pageContent += ".navbar{overflow:hidden; background-color:#333;}";
 	/* Style the navigation bar links */
-	pageContent += ".navbar a{float: left;display: block;color: white;text-align: center;padding: 14px 20px;text-decoration: none;}";
+	pageContent += ".navbar a{float:left; display:block; color:white; text-align:center; padding:14px 20px; text-decoration:none;}";
 	/* Right-aligned link */
-	pageContent += ".navbar a.right{float: right;}";
+	pageContent += ".navbar a.right{float:right;}";
 	/* Change color on hover */
-	pageContent += ".navbar a:hover{background-color: #ddd;color: black;}";
+	pageContent += ".navbar a:hover{background-color:#ddd; color:black;}";
 
 	pageContent += "</style>";
 	pageContent += "</head>";
 
-	pageContent += "<body>";
-	pageContent += "<div class = \"header\">";
-	pageContent += "<h1 id=\"weight\"></h1>";
-	pageContent += "<p>WLan-Waage</p>";
+	/* Headline */
+	pageContent += "<div class='footer'>";
+	pageContent += "<p style='font-size:16px;'>WLan Waage</p>";
 	pageContent += "</div>";
 
-	pageContent += "<div class = \"navbar\">";
-	pageContent += "<a href = \"#\">Tara</a>";
-	pageContent += "<a href = \"#\">Scale</a>";
-	pageContent += "<a href = \"#\">Admin</a>";
-	pageContent += "<a href = \"#\" class = \"right\">Home</a>";
+	/* Create two unequal columns that floats next to each other */
+	pageContent += "<div class='row'>";
+	pageContent += "<div class='leftcolumn'>";
+	pageContent += "<div class='header'>";
+	pageContent += "<h1 id='weight'></h1>";
+	pageContent += "</div>";
 	pageContent += "</div>";
 
+	pageContent += "<div class='rightcolumn'>";
+	pageContent += "<p>TEST</p>";
+	pageContent += "<p>TEST</p>";
+	pageContent += "</div>";
+	pageContent += "</div>";
+
+	pageContent += "<div class = 'navbar'>";
+	pageContent += "<a href = '#'>Tara</a>";
+	pageContent += "<a href = '#'>Scale</a>";
+	pageContent += "<a href = '#'>Admin</a>";
+	pageContent += "<a href = '#' class = 'right'>Home</a>";
+	pageContent += "</div>";
+
+	/* Footer */
+	pageContent += "<div class = 'footer'>";
+	pageContent += "<p>(c) 2019 WinBack GmbH</p>";
+	pageContent += "</div>";
+
+	/* Javascript */
 	pageContent += "<script>";
 	pageContent += "var myVar = setInterval(myTimer, 1000);";
 	pageContent += "function myTimer() {";
-	pageContent += "var xhttp = new XMLHttpRequest();";
-	pageContent += "xhttp.onreadystatechange = function() {";
-	pageContent += "if (this.readyState == 4 && this.status == 200) {";
-	pageContent += "document.getElementById(\"weight\").innerHTML = this.responseText;";
-	pageContent += "}";
-	pageContent += "};";
-	pageContent += "xhttp.open(\"GET\", \"xml\", true);";
+	pageContent += " var xhttp = new XMLHttpRequest();";
+	pageContent += " xhttp.onreadystatechange = function() {";
+	pageContent += "  if (this.readyState == 4 && this.status == 200) {";
+	pageContent += "   document.getElementById('weight').innerHTML = this.responseText;";
+	pageContent += "  }";
+	pageContent += " };";
+	pageContent += "xhttp.open('GET', 'xml', true);";
 	pageContent += "xhttp.send();";
 	pageContent += "}";
 	pageContent += "</script>";
@@ -79,8 +108,4 @@ void handleIndexPage(void) {
 	pageContent += "</body>";
 	pageContent += "</html>";
 	webserver.send(200, "text/html", pageContent);
-}
-
-void printweight() {
-	pageContent += "Gewicht " + String(weight) + " kg" + ".<br>\n";
 }
